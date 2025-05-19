2d4506474506d26d1570747f64cff2e3e356664c
Leonardo Guerrero <guerreroleonardo33@gmail.com>
2025-05-19 18:23:16 -0400

    make ctrl + enter autocomplete

--- init.lua
+++ init.lua
@@ -858,6 +858,7 @@ require('lazy').setup({
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+        ['<C-CR>'] = { 'select_and_accept' },
       },
 
       appearance = {
